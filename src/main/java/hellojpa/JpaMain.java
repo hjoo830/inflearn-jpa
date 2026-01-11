0cbd3776747fa052ef4bc5701457064a8cee4ad3
황효주 <hjoo830@naver.com>
2026-01-12 01:31:26 +0900

    JPQL

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
+import java.util.List;
+
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -21,7 +23,7 @@ public class JpaMain {
 //            em.persist(member);
 
 //            조회
-            Member findMember = em.find(Member.class, 1L);
+//            Member findMember = em.find(Member.class, 1L);
 //            System.out.println("findMember.id = " + findMember.getId());
 //            System.out.println("findMember.name = " + findMember.getName());
 
@@ -29,7 +31,17 @@ public class JpaMain {
 //            em.remove(findMember);
 
 //            수정
-            findMember.setName("HelloJPA");
+//            findMember.setName("HelloJPA");
+
+            // JPQL
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1) // 1번째 행부터
+                    .setMaxResults(10) // 10개 조회
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
